a34051ca0024633cce1654a5b50cba83cf61dfde
James Newman <n01490221@unf.edu>
2024-06-08 20:39:28 -0400

    updated binary search

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,11 +1,17 @@
-def binary_search(array, left, right, x):
-    while left < right:
-        mid = left + (right - left) / 2
-        if array[mid] == x:
-            return mid
-        elif array[mid] < x:
-            left = mid + 1
+def binary_search(nums, target):
+    
+    left = 0
+    right = len(nums) - 1
+    
+    while (left <= right):
+        middle = left + (right - left) // 2
+        
+        if nums[middle] == target:
+            return middle;
+        elif nums[middle] > target:
+            right = middle - 1
         else:
-            right = mid - 1
+            left = middle + 1
+    
+    return -1
 
-        return -1
